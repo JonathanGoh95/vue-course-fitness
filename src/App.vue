d9c87ff0cc975fcc626c694c66d5e7fc1db7871e
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-12-22 17:27:17 +0800

    Initialization and Basic Setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import Welcome from './components/Pages/Welcome.vue'
+  import Layout from './components/Layouts/Layout.vue'
+</script>
+
+<template>
+  <Layout>
+    <Welcome />
+  </Layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Pages/Welcome.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+    <section id="welcome">
+        <div class="benefits">
+            <h2>Complete this program<br/>if you want...</h2>
+            <div>
+                <p>✅ Follow a simple program with proven results.</p>
+                <p>✅ Get fit, healthy, strong or absolutely shredded.</p>
+                <p>✅ Learn more about Gym, Training and Technique.</p>
+            </div>
+        </div>
+        <div>
+            <h3>The Training Plan</h3>
+            <p>This training plan follows a structure known as the <strong>Bro Split</strong>.</p>
+            <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
+        </div>
+        <div class="card">
+            <h3>🔥 Your Challenge</h3>
+            <p>Complete all your workouts and track your progress along the way!</p>
+            <p>Do you accept?</p>
+            <button>Begin &rarr;</button>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    #welcome,
+    .challenge,
+    .benefits{
+        display: flex;
+        flex-direction: column;
+    }
+
+    #welcome{
+        gap: 1.5rem;
+    }
+
+    .benefits{
+        gap: 0.5rem;
+    }
+
+    .challenge{
+        gap: 0.25rem;
+    }
+</style>
